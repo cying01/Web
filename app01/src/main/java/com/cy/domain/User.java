67db6b3cf3d33481dabf4c8ef9a97d449a34695b
cying01 <178216333@qq.com>
2018-06-29 12:57:18 +0800

    init files

---
+++ app01/src/main/java/com/cy/domain/User.java
@@ -0,0 +1,103 @@
+package com.cy.domain;
+
+public class User {
+    private Integer uid;
+
+    private String realname;
+
+    private String comname;
+
+    private String email;
+
+    private Integer postcode;
+
+    private String qq;
+
+    private String wwang;
+
+    private String msn;
+
+    private Integer telephone;
+
+    private String phone;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname == null ? null : realname.trim();
+    }
+
+    public String getComname() {
+        return comname;
+    }
+
+    public void setComname(String comname) {
+        this.comname = comname == null ? null : comname.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(Integer postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq == null ? null : qq.trim();
+    }
+
+    public String getWwang() {
+        return wwang;
+    }
+
+    public void setWwang(String wwang) {
+        this.wwang = wwang == null ? null : wwang.trim();
+    }
+
+    public String getMsn() {
+        return msn;
+    }
+
+    public void setMsn(String msn) {
+        this.msn = msn == null ? null : msn.trim();
+    }
+
+    public Integer getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+}
